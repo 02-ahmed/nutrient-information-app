959f3fc46c59989a07c1da53927740302fabe4f7
02-ahmed <ahmedboabae2@gmail.com>
2024-06-28 11:50:03 +0000

    added a loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import  {  useEffect } from 'react'
-import axios from 'axios'
+import axios, { CanceledError } from 'axios'
 import { useState } from 'react'
 import Search from './components/Search'
 import Nutrients from './components/Nutrients';
 import './App.css';
+import { Controller } from 'react-hook-form';
 
 
 interface Food {
@@ -22,13 +23,17 @@ const App = () => {
   const [foods, setFoods] = useState<Food[]>([]);
   const [error, setError] = useState('');
   const [query, setQuery] = useState('');
-  const [hasSearched, setHasSearched] = useState(false)
+  const [hasSearched, setHasSearched] = useState(false);
+  const [isLoading, setLoading] = useState(false)
 
 
 
   const API_KEY = import.meta.env.VITE_API_KEY
 
   useEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true)
     axios.get<Food[]>("https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition", {
       headers: {
         'x-rapidapi-key': API_KEY,
@@ -36,12 +41,22 @@ const App = () => {
       },
       params: {
         query: query
-      }
+      },
+      signal:controller.signal
     })
-    .then((result) => setFoods(result.data))
-    .catch((error) => setError(error.message))
-
+    .then((result) => {
+      setFoods(result.data);
+      setLoading(false)
+    })
+    .catch((error) => {
+      if (error instanceof CanceledError) return
+      setError(error.message);
+      setLoading(false)
+    })
+    return () => controller.abort()
   }, [query])
+
+  
 
   useEffect(() => {
     setHasSearched(false)
@@ -55,7 +70,13 @@ const App = () => {
       </div>
       {error && <p className='text-danger'>{error}</p>}
       <Search onSearch={() => {setHasSearched(true)}}  handleSearch={(data) => {setQuery(data.search)}}/>
+      
       <div className="mb-2">
+        {isLoading && <div className="d-flex justify-content-center vh-100">
+            <div className="spinner-border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>}
         {hasSearched && foods.length === 0 ? <h5 className='text-center'>Nothing to show</h5>:""}
         <Nutrients foods={foods}/>
       </div>
